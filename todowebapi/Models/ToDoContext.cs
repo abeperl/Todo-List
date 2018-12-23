b91876f0597694edadfc4cb85a19d3bbbdc5d994
Abe Perl <abeperl@gmail.com>
2018-12-23 16:11:32 -0500

    Commit  Controllers and Models

---
+++ todowebapi/Models/ToDoContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace todowebapi.Models
+{
+    public partial class ToDoContext : DbContext
+    {
+        public ToDoContext()
+        {
+        }
+
+        public ToDoContext(DbContextOptions<ToDoContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Todo> Todo { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ToDo;Integrated Security=True;Connect Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Todo>(entity =>
+            {
+                entity.Property(e => e.Id)
+                    .HasColumnName("ID")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Detail)
+                    .HasMaxLength(500)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Dtcreated)
+                    .HasColumnName("dtcreated")
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Title)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Usercreated)
+                    .HasColumnName("usercreated")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
---
+++ todowebapi/Models/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace todowebapi.Models
+{
+    public partial class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Detail { get; set; }
+        public DateTime Dtcreated { get; set; }
+        public string Usercreated { get; set; }
+    }
+}
